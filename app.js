dfc7eb703f321b78ba2b873262353ea06f9d9689
HuynhVanDinh <dinh0375751606@gmail.com>
2023-04-01 20:44:01 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const comicsRouter = require("./app/routes/comic.rote");
+const comicsRouter = require("./app/routes/comic.route");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -14,4 +16,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/comics", comicsRouter);
 
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
