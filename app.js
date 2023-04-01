7dc1d19dd62613d1fbf8f8d8c3dad1097e7ce5e9
HuynhVanDinh <dinh0375751606@gmail.com>
2023-04-01 20:12:45 +0700

    Dinh nghia cac route cho tai nguyen Comic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const comicsRouter = require("./app/routes/comic.rote");
 const app = express();
 
 app.use(cors());
@@ -11,5 +11,7 @@ app.get("/", (req, res) => {
     message: "Wellcome to comic application.",
   });
 });
+
+app.use("/api/comics", comicsRouter);
 
 module.exports = app;
